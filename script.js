1f7af2a87564399741b67ec5f16ec87649ac5a5f
fernando <fernandodat600@gmail.com>
2025-01-28 16:59:46 -0300

    Exibindo a cotação da moeda

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const form = document.querySelector ("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector ("main footer")
+const description = document.getElementById("description")
 
 
 // Manipulando o input amount para receber somente números 
@@ -40,6 +41,8 @@ form.onsubmit = (event) => {
 //Função para converter a moeda 
 function convertCurrency(amount, price, symbol) {
   try {
+    description.textContent = `${symbol} 1 = ${price}`
+
     //Aplicar a classe que exibe o footer 
     footer.classList.add("show-result")
   } catch (error) {
